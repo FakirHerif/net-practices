7845d31f2a1401ea9c32f7c2ca3c4c012223a08e
FakirHerif <basdemir35@hotmail.com.tr>
2024-03-07 00:35:48 +0300

    5. practice added

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿// Bir sayının mükemmel sayı olup olmadığı kontrol eden algoritma yazınız.
+
+Console.Write("Bir sayı girin: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+for (int i = 1; i <= number / 2 ; i++)
+{
+    if (number % i == 0)
+    {
+        sum += i;
+    }
+}
+
+if (sum == number)
+{
+    Console.WriteLine("Girdiğiniz sayı " + number + " sayı mükemmel sayıdır");
+}
+else
+{
+    Console.WriteLine("Girdiğiniz sayı " + number + " mükemmel sayı değildir.");
+}
+
