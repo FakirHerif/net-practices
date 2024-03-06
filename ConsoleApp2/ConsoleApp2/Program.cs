73787a83788ca4dab353acd257c9a97adfba180e
FakirHerif <basdemir35@hotmail.com.tr>
2024-03-06 22:28:15 +0300

    2. practice added

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿// Kullanıcıdan kaç ürün almak istediğini soran,her ürünün fiyatını alarak toplam alışveriş tutarını hesaplayan bir algoritma yazınız.(Input için Scanner ve döngü için for döngüsü kullanınız)
+
+Console.Write("Kaç adet ürün almak istiyorsunuz? ");
+int orderNumber = Convert.ToInt32(Console.ReadLine());
+
+double totalPrice = 0;
+
+for (int i = 1; i <= orderNumber; i++)
+{
+    Console.Write("{0}. Ürünün Fiyatı: ", i);
+    double orderPrice = Convert.ToDouble(Console.ReadLine());
+    totalPrice += orderPrice;
+}
+
+Console.WriteLine("Toplam alışveriş tutarı: " + totalPrice);
